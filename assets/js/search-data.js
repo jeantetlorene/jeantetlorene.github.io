66f82825bb2e1b6cb3adece1c93a1b1ab19163c6
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-05-20 06:32:04 +0000

    Deploying to gh-pages from @ jeantetlorene/jeantetlorene.github.io@1c4eb2889c3ca9e97c9f19e6d35a89a16dbad76d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,7 +494,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%6F%72%65%6E%65@%61%69%6D%73.%61%63.%7A%61", "_blank");
+        },
+      },{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/lorenejeantet.bsky.social", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/jeantetlorene", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -504,18 +518,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/lorène-jeantet-2241a6b3", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=jKKBYZIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
