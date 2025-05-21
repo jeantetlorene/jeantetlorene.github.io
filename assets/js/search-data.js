71e72e49d4303e19aa5ad1c22e7eaec48c995407
jeantetlorene <105348746+jeantetlorene@users.noreply.github.com>
2025-05-21 10:01:48 +0000

    Deploying to gh-pages from @ jeantetlorene/jeantetlorene.github.io@efbd57209457d47f57ba2113d76a6708f8867331 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "The GitHub repositories contain the code associated with some published article. Each repository provides a description of the main results along with the relevant code.",
+          description: "The GitHub repositories contain the code associated with some published articles. Each repository provides a description of the main results along with the relevant code.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Repositories/";
